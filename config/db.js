40097ea8aca6d3d6cae60bc5a5f6bee8fcbb0995
Harsh <doingonlygoodthings@gmail.com>
2025-09-26 07:00:00 +0000

    5th fixing local to cloud

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/attendanceDB", {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("✅ MongoDB Connected");
+    console.log("✅ Connected to MongoDB Atlas");
   } catch (error) {
     console.error("❌ MongoDB Connection Failed:", error.message);
     process.exit(1);

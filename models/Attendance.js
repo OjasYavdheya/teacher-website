5865eec8b25fcfbd531a5451fe6a0dad921c986b
Harsh <doingonlygoodthings@gmail.com>
2025-09-28 09:28:55 +0000

    21th attendancescehma

--- models/Attendance.js
+++ models/Attendance.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const attendanceSchema = new mongoose.Schema({
   studentId: { type: mongoose.Schema.Types.ObjectId, ref: "Student", required: true },
-  date: { type: Date, required: true } // format: YYYY-MM-DD
+  date: { type: Date, default: Date.now },
+  status: { type: String, enum: ["Present", "Absent"], required: true }
 });
 
 module.exports = mongoose.model("Attendance", attendanceSchema);
